1749f0950b9605d8d2029f09cd432bd3f2c8b656
chsidev <127224337+chsidev@users.noreply.github.com>
2023-12-05 12:56:10 +0700

    Cleanup old print statements

--- migrations/3_deploy_airdrop.js
+++ migrations/3_deploy_airdrop.js
@@ -4,4 +4,4 @@ module.exports = async (deployer, network, addresses) => {
   let deployAddress = addresses[0];
   await deployer.deploy(Airdrop, {gas: 5000000, from: deployAddress });
   const airdrop = await Airdrop.deployed();
-};
+};# Change 1 on 2023-12-05
